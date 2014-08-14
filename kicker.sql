ccc2febe66f2085aed5cba1e283ac7e21d869dea
Lutz Bicker <lutz.bicker@googlemail.com>
2014-08-14 11:54:06 +0200

    Initial Commit

---
+++ kicker.sql
@@ -0,0 +1,14 @@
+
+
+CREATE TABLE IF NOT EXISTS `kk_spieler2014_2015` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `verein` varchar(100) NOT NULL,
+  `position` varchar(100) NOT NULL,
+  `vk_preis` decimal(10,2) NOT NULL,
+  `ek_preis` decimal(10,2) NOT NULL,
+  `kaeufer` varchar(100) NOT NULL,
+  `note` decimal(10,2) NOT NULL,
+  `punkte` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=505 ;
